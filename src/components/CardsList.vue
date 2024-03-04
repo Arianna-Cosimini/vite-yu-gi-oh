c9d0aa5170f29814727b951cf79f5e73af8373bd
arianna-cosimini <arianna.cosimini@gmail.com>
2024-03-04 17:35:24 +0100

    stilizzaro cards in pagine e stampato immagine, nome e archetype

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -16,16 +16,37 @@ export default {
 
 <template>
 
-    <div class="container">
+    <div class="container-big">
 
-        <div class="found-cards">
-            <span>
-               Founds {{ cards.length }} Cards
-            </span>
-        </div>
+        <div class="container-small">
+            <div class="found-cards">
+                <span>
+                    Founds {{ cards.length }} Cards
+                </span>
+            </div>
+    
+            <div class="cards">
+                <div v-for="currentCard in cards" class="card">
+                    <div class="img-card">
+                        <img :src="currentCard.card_images[0].image_url" alt="">
+    
+                    </div>
+                    <div class="text-card">
+                        <div class="text-name">
+                            <span class="name">{{ currentCard.name }}</span>
+                            
+                        </div>
+                        
+                        <div class="text-archetype">
+                            <span class="archetipe">{{ currentCard.archetype }}</span>
+    
+                        </div>
 
-        <div v-for="currentCard in cards" class="cards">
-            {{ currentCard.name }}
+                    </div>
+                </div>
+    
+            </div>
+
         </div>
 
     </div>
@@ -34,16 +55,60 @@ export default {
 </template>
 
 <style lang="scss">
-.container {
-    max-width: 1200px;
+.container-big {
+    max-width: 1600px;
     margin: 0 auto;
     background-color: #fff;
     color: black;
 
-    .found-cards{
+    padding: 50px 0;
+
+    .found-cards {
         background-color: #212529;
         padding: 20px 10px;
         color: #fff;
+    }
+}
+
+.container-small{
+    max-width:1500px;
+    margin: 0 auto;
+}
+
+.cards {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25px;
+
+    .card {
+        width: calc(100% / 5 - 25px / 5 * 4);
+        display: flex;
+        gap: 20px;
+        align-items: center;
+        flex-direction: column;
+        justify-content: space-between;
+
+        background-color: #d48f38;
+
+        padding-bottom: 20px;
+
+        img {
+            width: 100%;
+        }
+
+        .text-card{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            
+        }
+
+        .text-name{
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+        }
+
     }
 }
 </style>
